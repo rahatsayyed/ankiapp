cabcda9353e9ab34a3d089060128924b49b6dfd4
Rahat Sayyed <219821091+rahat-sayyed@users.noreply.github.com>
2025-10-06 10:36:02 +0530

    fix: create set

--- app/(modals)/set/create.tsx
+++ app/(modals)/set/create.tsx
@@ -1,15 +1,14 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
-import { defaultStyleSheet } from '@/constants/Styles';
-import { TextInput } from 'react-native-gesture-handler';
-import { addToFavorites, createSet } from '@/data/api';
-import { useRouter } from 'expo-router';
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import React, { useState } from "react";
+import { defaultStyleSheet } from "@/constants/Styles";
+import { addToFavorites, createSet } from "@/data/api";
+import { useRouter } from "expo-router";
 
 const Page = () => {
   const router = useRouter();
   const [information, setInformation] = useState({
-    title: '',
-    description: '',
+    title: "",
+    description: "",
   });
 
   // Create set, add new favorite and go back
@@ -21,22 +20,34 @@ const Page = () => {
 
   return (
     <>
-      <View style={[defaultStyleSheet.container, { marginTop: 20, marginHorizontal: 16 }]}>
+      <View
+        style={[
+          defaultStyleSheet.container,
+          { marginTop: 20, marginHorizontal: 16 },
+        ]}
+      >
         <TextInput
           style={defaultStyleSheet.input}
           placeholder="Title"
           value={information.title}
-          onChangeText={(text) => setInformation({ ...information, title: text })}
+          onChangeText={(text) =>
+            setInformation({ ...information, title: text })
+          }
         />
         <TextInput
           style={defaultStyleSheet.input}
           placeholder="Description"
           value={information.description}
-          onChangeText={(text) => setInformation({ ...information, description: text })}
+          onChangeText={(text) =>
+            setInformation({ ...information, description: text })
+          }
         />
       </View>
-      <View style={{ alignItems: 'center' }}>
-        <TouchableOpacity style={defaultStyleSheet.bottomButton} onPress={onCreateSet}>
+      <View style={{ alignItems: "center" }}>
+        <TouchableOpacity
+          style={defaultStyleSheet.bottomButton}
+          onPress={onCreateSet}
+        >
           <Text style={defaultStyleSheet.buttonText}>Create Set</Text>
         </TouchableOpacity>
       </View>
